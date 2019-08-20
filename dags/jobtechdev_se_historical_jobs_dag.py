3bca8f61b9a1328cfde773c7671b400221bfea1a
Gabriel <eu@gabrielf.com>
2019-08-19 21:46:22 -0300

    Creating the first version of the dags only with pseudo code

---
+++ dags/jobtechdev_se_historical_jobs_dag.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta
+import os
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
+                                LoadDimensionOperator, DataQualityOperator)
+from helpers import SqlQueries
+
+default_args = {
+    'owner': 'gabriel',
+    'start_date': datetime(2019, 1, 12),
+    'depends_on_past': False,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=300),
+    'catchup': False
+}
+
+dag = DAG('jobtechdev_se_historical_jobs_dag',
+          default_args=default_args,
+          description='Load the jobs dataset and insert into Redshift',
+          schedule_interval='0 * * * *'
+        )
+
+# Re-Create the staging table
+# Stage all the JSON data from diverses years with a COPY command (paralellize the years? Yes!)
+# Run the query to copy the data from the staging table to the fact/dimensions table
+#   | -> First the dimensions (tags and companies)
+#   | -> Then the fact (job_vacancies)
