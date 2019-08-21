3bd2af39bcb7bd92710489c12ff2c49ac93ff601
Gabriel <eu@gabrielf.com>
2019-08-20 21:26:13 -0300

    wip: making jobtechdev.se historical jobs dag stop prompting errors

--- dags/jobtechdev_se_historical_jobs_dag.py
+++ dags/jobtechdev_se_historical_jobs_dag.py
@@ -2,8 +2,7 @@ from datetime import datetime, timedelta
 import os
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
-                                LoadDimensionOperator, DataQualityOperator)
+from airflow.operators import (StageCsvToRedshiftOperator, PostgresOperator)
 from helpers import SqlQueries
 
 default_args = {
@@ -20,6 +19,24 @@ dag = DAG('jobtechdev_se_historical_jobs_dag',
           description='Load the jobs dataset and insert into Redshift',
           schedule_interval='0 * * * *'
         )
+
+recreate_staging_table = PostgresOperator(
+    task_id="recreate_staging_dice_com_jobs_table",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=SqlQueries.recreate_staging_dice_com_jobs_table
+)
+
+stage_events_to_redshift = StageCsvToRedshiftOperator(
+    task_id='Stage_events',
+    dag=dag,
+    table="staging_events",
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    s3_bucket="udacity-dend",
+    s3_key="log_data",
+    json_path="s3://udacity-dend/log_json_path.json"
+)
 
 # Re-Create the staging table
 # Stage all the JSON data from diverses years with a COPY command (paralellize the years? Yes!)
