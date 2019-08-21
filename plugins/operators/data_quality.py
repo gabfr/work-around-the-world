0ed8695948cdf9f6f25056e29e0c6aa45228cb80
Gabriel <eu@gabrielf.com>
2019-08-21 18:26:50 -0300

    Now we can defined WHERE parameters on our data quality op

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -10,16 +10,21 @@ class DataQualityOperator(BaseOperator):
     def __init__(self,
                  redshift_conn_id="",
                  tables=[],
+                 where_parameters=None,
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
         self.tables = tables
+        self.where_parameters = where_parameters
 
     def execute(self, context):
         redshift_hook = PostgresHook(self.redshift_conn_id)
+        where_params = ''
+        if self.where_parameters is not None:
+            where_params = f"WHERE {self.where_parameters}"
         for table in self.tables:
-            records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table}")
+            records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table} {where_params}")
             if len(records) < 1 or len(records[0]) < 1:
                 raise ValueError(f"Data quality check failed. {table} returned no results")
             num_records = records[0][0]
