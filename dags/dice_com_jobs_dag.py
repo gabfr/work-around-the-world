6a709827fcd4ba97369ba2df0caaf1cc9ff61ba1
Gabriel <eu@gabrielf.com>
2019-08-21 12:14:46 -0300

    creating two more steps of our dice com jobs dag:  - dimension tags table upsert query  - fact job_vacancies table upsert query

--- dags/dice_com_jobs_dag.py
+++ dags/dice_com_jobs_dag.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 import os
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators import (StageCsvToRedshiftOperator, PostgresOperator, LoadDimensionOperator)
+from airflow.operators import (StageCsvToRedshiftOperator, PostgresOperator, LoadDimensionOperator, LoadFactOperator)
 from helpers import SqlQueries
 
 default_args = {
@@ -17,7 +17,7 @@ default_args = {
 dag = DAG('dice_com_jobs_dag',
           default_args=default_args,
           description='Load the jobs dataset and insert into Redshift',
-          schedule_interval='0 * * * *'
+          schedule_interval='@once'
         )
 
 recreate_staging_table = PostgresOperator(
@@ -46,6 +46,22 @@ upsert_companies_dimension_table = LoadDimensionOperator(
     select_query=SqlQueries.select_companies_from_dice_jobs_staging_table
 )
 
+upsert_tags_dimension_table = LoadDimensionOperator(
+    task_id='upsert_tags_dimension_table',
+    dag=dag,
+    table='tags',
+    redshift_conn_id="redshift",
+    select_query=SqlQueries.select_tags_from_dice_jobs_staging_table
+)
+
+upsert_job_vacancies_fact_table = LoadFactOperator(
+    task_id='upsert_job_vacancies_fact_table',
+    dag=dag,
+    table='job_vacancies',
+    redshift_conn_id="redshift",
+    select_query=SqlQueries.select_job_vacancies_from_dice_jobs_staging_table
+)
+
 # Re-Create the staging table
 # Stage the data with a COPY command
 # Run the query to copy the data from the staging table to the fact/dimensions table
@@ -54,3 +70,5 @@ upsert_companies_dimension_table = LoadDimensionOperator(
 
 recreate_staging_table >> stage_dice_com_jobs
 stage_dice_com_jobs >> upsert_companies_dimension_table
+stage_dice_com_jobs >> upsert_tags_dimension_table
+stage_dice_com_jobs >> upsert_job_vacancies_fact_table
--- plugins/helpers/sql_queries.py
+++ plugins/helpers/sql_queries.py
@@ -72,3 +72,52 @@ class SqlQueries:
             staging_dice_com_jobs
         where 1 not in (select id from companies);
     """)
+
+    select_tags_from_dice_jobs_staging_table = ("""
+        with NS AS (
+          SELECT 1 as n UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL 
+          SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9 UNION ALL SELECT 10 UNION ALL 
+          SELECT 11 UNION ALL SELECT 12 UNION ALL SELECT 13 UNION ALL SELECT 14 UNION ALL SELECT 15 UNION ALL 
+          SELECT 16 UNION ALL SELECT 17 UNION ALL SELECT 18 UNION ALL SELECT 19 UNION ALL SELECT 20 UNION ALL 
+          SELECT 21 UNION ALL SELECT 22 UNION ALL SELECT 23 UNION ALL SELECT 24 UNION ALL SELECT 25 UNION ALL 
+          SELECT 26 UNION ALL SELECT 27 UNION ALL SELECT 28 UNION ALL SELECT 29 UNION ALL SELECT 30 UNION ALL 
+          SELECT 31 UNION ALL SELECT 32 UNION ALL SELECT 33 UNION ALL SELECT 34 UNION ALL SELECT 35 UNION ALL 
+          SELECT 36 UNION ALL SELECT 37 UNION ALL SELECT 38 UNION ALL SELECT 39 UNION ALL SELECT 40 UNION ALL 
+          SELECT 41 UNION ALL SELECT 42 UNION ALL SELECT 43 UNION ALL SELECT 44 UNION ALL SELECT 45 UNION ALL 
+          SELECT 46 UNION ALL SELECT 47 UNION ALL SELECT 48 UNION ALL SELECT 49 UNION ALL SELECT 50 UNION ALL 
+          SELECT 51 UNION ALL SELECT 52 UNION ALL SELECT 53 UNION ALL SELECT 54 UNION ALL SELECT 55 UNION ALL 
+          SELECT 56 UNION ALL SELECT 57 UNION ALL SELECT 58 UNION ALL SELECT 59 UNION ALL SELECT 60
+        )
+        select distinct 
+            trim(split_part(sector, ',', NS.n)) as "tag"
+        from NS
+        inner join staging_dice_com_jobs S ON NS.n <= REGEXP_COUNT(S.sector, ',') + 1
+        where LEN("tag") <= 50 and "tag" not in (select "tag" from tags)
+    """)
+
+    select_job_vacancies_from_dice_jobs_staging_table = ("""
+        select distinct  
+            concat(row_number() over (partition by 1), concat('-', 'dice_com')) as id,
+            'dice_com' as provider_id,
+            page_url as remote_id_on_provider,
+            page_url as remote_url,
+            location as location,
+            null as currency_code,
+            REPLACE(TRIM(regexp_replace(translate(
+                LOWER(organization),
+                'áàâãäåāăąèééêëēĕėęěìíîïìĩīĭḩóôõöōŏőùúûüũūŭůäàáâãåæçćĉčöòóôõøüùúûßéèêëýñîìíïş',
+                'aaaaaaaaaeeeeeeeeeeiiiiiiiihooooooouuuuuuuuaaaaaaeccccoooooouuuuseeeeyniiiis'
+            ), '[^a-z0-9\-]+', ' ')),' ', '-') as company_id,
+            organization as company_name,
+            job_title as title,
+            job_description as description,
+            sector as tags,
+            null as salary,
+            null as salary_max,
+            null as salary_frequency,
+            0 as has_relocation_package,
+            TO_TIMESTAMP(date_added, 'YYYY-MM-DD') as published_at
+        from 
+            staging_dice_com_jobs
+        where page_url not in (select remote_id_on_provider from job_vacancies where provider_id = 'dice_com')
+    """)
