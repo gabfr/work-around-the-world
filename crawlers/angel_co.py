7c85656c5481f050ed7779ec5bfe70056bd2afea
Gabriel <eu@gabrielf.com>
2019-09-12 10:41:19 -0300

    feature: going further on the jobs list scraping Played around enough to designed the logic to save the scraped startup jobs listing

--- crawlers/angel_co.py
+++ crawlers/angel_co.py
@@ -38,21 +38,7 @@ def lazy_get_element(driver, css_selector, timeout=30):
     return WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.CSS_SELECTOR, css_selector)))
 
 
-def main():
-    global ANGELCO_EMAIL, ANGELCO_PASSWORD
-
-    config_parse_file()
-
-    print("Email: " + ANGELCO_EMAIL)
-    print("Password: " + ANGELCO_PASSWORD)
-
-    incognito_mode = Options()
-    incognito_mode.add_argument('--incognito')
-
-    driver = selenium_create_driver(options=incognito_mode)
-
-    driver.get('https://angel.co')
-
+def accept_cookies(driver):
     driver.implicitly_wait(5)
 
     accept_cookies_button = lazy_get_element(driver, '.c-button.c-button--blue')
@@ -60,8 +46,8 @@ def main():
     if accept_cookies_button is not None and accept_cookies_btn_is_interactable:
         accept_cookies_button.click()
 
-    driver.implicitly_wait(1)
 
+def do_login(driver):
     login_button = lazy_get_element(driver, 'a.auth.login')
     if login_button is not None:
         login_button.click()
@@ -85,6 +71,62 @@ def main():
     if login_form_button is None:
         print('Cant find the login form button? Cant follow with the script')
     login_form_button.click()
+
+
+def clean_all_filters(driver, selector):
+    """
+    Had to go to a recursive approach to clean all filters as our element tree changes each time we delete
+    a filter
+    :param driver:
+    :param selector:
+    :return:
+    """
+    filters_buttons = driver.find_elements_by_css_selector(selector)
+
+    if not filters_buttons:
+        return
+
+    filters_buttons[0].click()
+    driver.implicitly_wait(1)
+
+    if len(filters_buttons) > 1:
+        clean_all_filters(driver, selector)
+
+
+def main():
+    global ANGELCO_EMAIL, ANGELCO_PASSWORD
+
+    config_parse_file()
+
+    print("Email: " + ANGELCO_EMAIL)
+    print("Password: " + ANGELCO_PASSWORD)
+
+    incognito_mode = Options()
+    incognito_mode.add_argument('--incognito')
+
+    driver = selenium_create_driver(options=incognito_mode)
+
+    driver.get('https://angel.co')
+
+    driver.implicitly_wait(5)
+
+    accept_cookies(driver)
+
+    do_login(driver)
+
+    lazy_get_element(driver, '.remove-filter.delete')  # wait until the remove filter buttons appear
+    # then, find all remove filter buttons
+
+    clean_all_filters(driver, '.remove-filter.delete')
+
+    # after cleaning all filter's
+
+    # Waiting for the startup div that holds the jobs listing
+    lazy_get_element(
+        driver,
+        '#startups_content > .job_listings.browse_startups > .find.g-module.startup-container > div'
+    )
+
 
     input('prompt')
 
