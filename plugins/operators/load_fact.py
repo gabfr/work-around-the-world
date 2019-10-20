47e2edde58606afa727f34c0b9457d575ff9a3f9
Gabriel <eu@gabrielf.com>
2019-10-19 21:44:51 -0300

    refactor: should've be done at from redshift to postgresql

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -15,21 +15,21 @@ class LoadFactOperator(BaseOperator):
                  # Define your operators params (with defaults) here
                  # Example:
                  # conn_id = your-connection-name
-                 redshift_conn_id,
+                 pgsql_conn_id,
                  table,
                  select_query,
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
 
-        self.redshift_conn_id = redshift_conn_id
+        self.pgsql_conn_id = pgsql_conn_id
         self.table = table
         self.select_query = select_query
 
     def execute(self, context):
-        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        pgsql = PostgresHook(postgres_conn_id=self.pgsql_conn_id)
 
-        redshift.run(LoadFactOperator.insert_into_stmt.format(
+        pgsql.run(LoadFactOperator.insert_into_stmt.format(
             table=self.table,
             select_query=self.select_query
         ))
