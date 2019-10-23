ae1453f38dad862df8ceae1681da6c465af11f69
Gabriel <eu@gabrielf.com>
2019-10-23 10:17:02 -0300

    refactor: configuring catchup in the right place DAG args

--- dags/dice_com_jobs_dag.py
+++ dags/dice_com_jobs_dag.py
@@ -13,14 +13,14 @@ default_args = {
     'start_date': datetime(2019, 8, 21),
     'depends_on_past': False,
     'retries': 1,
-    'retry_delay': timedelta(seconds=300),
-    'catchup': False
+    'retry_delay': timedelta(seconds=300)
 }
 
 dag = DAG('dice_com_jobs_dag',
           default_args=default_args,
           description='Load the jobs dataset and insert into Redshift',
-          schedule_interval='@once'
+          schedule_interval='@once',
+          catchup=False
         )
 
 recreate_staging_dice_com_jobs_table = PostgresOperator(
